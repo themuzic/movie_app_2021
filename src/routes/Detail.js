2149565166185645dadac2e3bebed48433341875
Jaekwang Jeon <masta_jk@naver.com>
2021-09-08 14:31:47 +0900

    ok

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -14,30 +14,41 @@ class Detail extends React.Component{
 
   render(){
     const {location} = this.props;
-    return (
-      <>
-      <section className="container">
-        <div className="back">
-          <Link to="/">Back</Link>
-        </div>
-        <div className="movies">
-          <div className="movie detail">
-            <img className="detail" src={location.state.poster} alt={location.state.title} title={location.state.title}></img>
-            <div className="movie__data detail">
-              <h3 className="movie__title detail">{location.state.title}</h3>
-              <h5 className="movie__year detail">{location.state.year}</h5>
-              <ul className="movie__genres detail">
-                  {location.state.genres.map((genre, idx) => (
-                      <li className="genres__genre detail" key={idx}>{genre}</li>
-                  ))}
-              </ul>
-              <p className="movie_summary detail">{location.state.summary}</p>
-            </div>
-          </div>          
-        </div>
-      </section>
-    </>
-    )
+    //console.log(location);
+
+    if(location.state === undefined){
+      return (
+        <section className="container">
+          <span>Access Denied</span>
+        </section>
+      )
+    }else{
+      return (
+        <>
+        <section className="container">
+          <div className="back">
+            <Link to="/">Back</Link>
+          </div>
+          <div className="movies">
+            <div className="movie detail">
+              <img className="detail" src={location.state.poster} alt={location.state.title} title={location.state.title}></img>
+              <div className="movie__data detail">
+                <h3 className="movie__title detail" onClick={() => {alert(`${location.state.title}`);}}>{location.state.title}</h3>
+                <h5 className="movie__year detail">{location.state.year}</h5>
+                <ul className="movie__genres detail">
+                    {location.state.genres.map((genre, idx) => (
+                        <li className="genres__genre detail" key={idx}>{genre}</li>
+                    ))}
+                </ul>
+                <p className="movie_summary detail">{location.state.summary}</p>
+              </div>
+            </div>          
+          </div>
+        </section>
+      </>
+      )
+    }
+    
   }
 }
 
